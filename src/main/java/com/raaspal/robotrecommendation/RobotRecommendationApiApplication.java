e30d4020e21a32c446f21f7a1739ee7d2db9d414
Swan Htet Aung <swanhtetag09@gmail.com>
2026-05-21 13:05:59 +0700

    Initial Commit

---
+++ src/main/java/com/raaspal/robotrecommendation/RobotRecommendationApiApplication.java
@@ -0,0 +1,13 @@
+package com.raaspal.robotrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotRecommendationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotRecommendationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raaspal/robotrecommendation/RobotRecommendationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raaspal.robotrecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotRecommendationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
